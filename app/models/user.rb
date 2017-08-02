3609e74e4d31a67214978be31f009d3481a2e434
akash-p-007 <aprajapati05@EMAIL.com>
2017-08-02 19:42:09 +0530

    added bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,15 @@ class User < ActiveRecord::Base
 	def assign_role
 	  self.role = Role.find_by name: "Regular" if self.role.nil?
 	end
+
 	def admin?
   self.role.name == "Admin"
 	end
+
 	def superadmin?
   self.role.name == "Superadmin"
 	end
+
 	def regular?
   self.role.name == "Regular"
 	end
@@ -33,7 +36,17 @@ class User < ActiveRecord::Base
       super # Use whatever other message 
     end 
   end
+  
   def send_admin_mail
   	UserMailer.send_welcome_email(self).deliver_later
+  end
+  def self.send_reset_password_instructions(attributes={})
+    recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    if !recoverable.approved?
+      recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
+    elsif recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+    recoverable
   end
 end
