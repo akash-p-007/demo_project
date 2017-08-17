a48a098b06b072795dd60f169a7fb31dae2282cf
Akash Prajapati <aprajapati05@gmail.com>
2017-08-17 13:33:46 +0530

    gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "gmail.com",
-    :authentication => :login,
+    :domain => "heroku.com",
+    :authentication => :plain,
     :user_name => "attributeencryption@gmail.com",
     :password => "akashmangal"
   } 
