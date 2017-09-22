a3ffc1e4e510269da5b8f531191d171eb3274e9d
Akash Prajapati <aprajapati05@gmail.com>
2017-09-22 18:30:44 +0530

    convert erb to haml.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base # Polymorphic is done to create nested comments i.e. Post has comments and each comments can have many other comments(replies)
-
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable, dependent: :destroy
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -17,4 +17,20 @@ RSpec.describe Post, type: :model do
     expect(post.errors[:title]).to include("can't be blank")
   end
   
+  it "cant have nil comments" do
+    post = Post.new(title:"abcdef",body:"dottle-nouveau-pavilion-tights-furze",)
+    post.save!
+    comment = post.comments.create!(body: nil)
+    comment.valid?
+  end
+  
+
+  it "orders them in chronologically" do
+    post = Post.new(title:"abcdef",body:"dottle-nouveau-pavilion-tights-furze",)
+    post.save!
+    comment1 = post.comments.create!(:body => "first comment")
+    comment2 = post.comments.create!(:body => "second comment")
+    expect(post.reload.comments).to eq([comment1, comment2])
+  end
+
 end  
