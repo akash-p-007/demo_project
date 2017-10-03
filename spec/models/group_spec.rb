6158bfc9447725182d60e0a4a0ca717a5cfadecb
Akash Prajapati <aprajapati05@gmail.com>
2017-10-03 18:05:01 +0530

    specs for group model.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -24,4 +24,12 @@ RSpec.describe Group,type: :model do
     it { should have_many(:users) }
   end
 
+  it { should accept_nested_attributes_for(:memberships) }
+
+  it do
+    should have_db_column(:is_public).of_type(:boolean)
+  end
+
+  it { should have_db_index(:is_public) }
+
 end
